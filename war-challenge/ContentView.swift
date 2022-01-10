c56ff44be47e826141712685459c20b433711d56
karma <karmatashi954@gmail.com>
2022-01-10 18:19:50 +0545

    initial commit

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -9,13 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(){
+            Image("background").ignoresSafeArea()
+            
+            VStack(){
+                Spacer()
+                Image("logo")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Spacer()
+                HStack(){
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                VStack(){
+                    
+                    HStack(){
+                        Spacer()
+                        Text("Player")
+                            .fontWeight(.bold)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            
+                            
+                        Spacer()
+                        Text("CPU")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                    }
+                    HStack(){
+                        Spacer()
+                        Text("0")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                        Text("0")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                    }
+                }
+                
+                Spacer()
+            }
+            
+            
+        }
+        
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
+            
     }
 }
